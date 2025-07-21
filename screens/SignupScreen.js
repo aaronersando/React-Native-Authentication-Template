f48f10f5d42849b0d49111a903a75396a0172cc3
aaronersando <agersando@gmail.com>
2025-07-22 02:37:19 +0800

    Implement signup functionality with loading state

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleSignup({ email, password }) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={handleSignup} />;
 }
 
 export default SignupScreen;
